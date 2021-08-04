60213629c1f3356f099719334d441bb617d1631a
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2021-08-04 11:06:38 +0200

    Tests para todos los modelos

--- test/test_pytorch.py
+++ test/test_pytorch.py
@@ -28,14 +28,15 @@ class MyTestCase(unittest.TestCase):
         tra_size = int(len(mit_bih) * 0.8)
         tst_size = len(mit_bih) - tra_size
         train, test = torch.utils.data.random_split(mit_bih, [tra_size, tst_size])
-        train_loader = torch.utils.data.DataLoader(train, batch_size=256, num_workers=0)
-        test_loader = torch.utils.data.DataLoader(test, batch_size=256, num_workers=0)
+        train_loader = torch.utils.data.DataLoader(train, batch_size=256, num_workers=8)
+        test_loader = torch.utils.data.DataLoader(test, batch_size=256, num_workers=8)
         # train_sampler = SubsetRandomSampler(range(len(mit_bih)))
         # train_loader = torch.utils.data.DataLoader(mit_bih, batch_size=256, sampler=train_sampler, num_workers=8)
 
 
         # Train
-        trainer = pl.Trainer(gpus=1, max_epochs=epochs)
+        #trainer = pl.Trainer(gpus=1, max_epochs=epochs)
+        trainer = pl.Trainer(max_epochs=epochs)
         trainer.fit(model=model, train_dataloader=train_loader)
         if evaluate_test:
             test_results = trainer.test(test_dataloaders=test_loader)
@@ -96,6 +97,7 @@ class MyTestCase(unittest.TestCase):
         acc = self.trainModel(model, 1000, 30)
         assert 1.0 >= acc >= 0.8
 
+    @unittest.skip
     def test_YildirimOzal(self):
         model = YildirimOzal(
             input_shape=(1,1000),
@@ -116,6 +118,108 @@ class MyTestCase(unittest.TestCase):
         acc = self.trainModel(model, 1000, 30, False, True)
 
         assert 1.0 >= acc >= 0.8
+
+    #####################################################
+
+    @unittest.skip
+    def test_KhanZulfiqar(self):
+        model = KhanZulfiqar(in_features=1,
+                          loss=nn.CrossEntropyLoss(),
+                          metrics={"acc": acc_from_logits},
+                          optimizer=torch.optim.Adam,
+                          lr=0.001
+                          )
+        acc = self.trainModel(model, 1000, 30)
+        assert 1.0 >= acc >= 0.8
+
+    @unittest.skip
+    def test_ZhengZhenyu(self):
+        model = ZhengZhenyu(in_features=1,
+                          loss=nn.CrossEntropyLoss(),
+                          metrics={"acc": acc_from_logits},
+                          optimizer=torch.optim.Adam,
+                          lr=0.001
+                          )
+        acc = self.trainModel(model, 1000, 30)
+        assert 1.0 >= acc >= 0.8
+
+    @unittest.skip
+    def test_WangKejun(self):
+        model = WangKejun(in_features=1,
+                          loss=nn.CrossEntropyLoss(),
+                          metrics={"acc": acc_from_logits},
+                          optimizer=torch.optim.Adam,
+                          lr=0.001
+                          )
+        acc = self.trainModel(model, 1000, 30)
+        assert 1.0 >= acc >= 0.8
+
+    @unittest.skip
+    def test_ChenChen(self):
+        model = ChenChen(in_features=1,
+                          loss=nn.CrossEntropyLoss(),
+                          metrics={"acc": acc_from_logits},
+                          optimizer=torch.optim.Adam,
+                          lr=0.001
+                          )
+        acc = self.trainModel(model, 1000, 30)
+        assert 1.0 >= acc >= 0.8
+
+    @unittest.skip
+    def test_KimTaeYoung(self):
+        model = KimTaeYoung(in_features=1,
+                          loss=nn.CrossEntropyLoss(),
+                          metrics={"acc": acc_from_logits},
+                          optimizer=torch.optim.Adam,
+                          lr=0.001
+                          )
+        acc = self.trainModel(model, 1000, 30)
+        assert 1.0 >= acc >= 0.8
+
+    @unittest.skip
+    def test_GenMinxing(self):
+        model = GenMinxing(in_features=1000,
+                          loss=nn.CrossEntropyLoss(),
+                          metrics={"acc": acc_from_logits},
+                          optimizer=torch.optim.Adam,
+                          lr=0.001
+                          )
+        acc = self.trainModel(model, 1000, 30)
+        assert 1.0 >= acc >= 0.8
+
+    @unittest.skip
+    def test_FuJiangmeng(self):
+        model = FuJiangmeng(in_features=1,
+                          loss=nn.CrossEntropyLoss(),
+                          metrics={"acc": acc_from_logits},
+                          optimizer=torch.optim.Adam,
+                          lr=0.001
+                          )
+        acc = self.trainModel(model, 1000, 30)
+        assert 1.0 >= acc >= 0.8
+
+    @unittest.skip
+    def test_ShiHaotian(self):
+        model = ShiHaotian(in_features=1,
+                          loss=nn.CrossEntropyLoss(),
+                          metrics={"acc": acc_from_logits},
+                          optimizer=torch.optim.Adam,
+                          lr=0.001
+                          )
+        acc = self.trainModel(model, 1000, 30)
+        assert 1.0 >= acc >= 0.8
+
+    @unittest.skip
+    def test_HuangMeiLing(self):
+        model = HuangMeiLing(in_features=1,
+                          loss=nn.CrossEntropyLoss(),
+                          metrics={"acc": acc_from_logits},
+                          optimizer=torch.optim.Adam,
+                          lr=0.001
+                          )
+        acc = self.trainModel(model, 1000, 30)
+        assert 1.0 >= acc >= 0.8
+
 
 if __name__ == '__main__':
     unittest.main()
