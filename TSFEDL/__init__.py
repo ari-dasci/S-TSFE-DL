bba1b2566671a5f93d9b2a3fec8a749e4504f9e6
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2022-02-09 18:51:59 +0100

    Archivo config init module

---
+++ TSFEDL/__init__.py
@@ -0,0 +1,179 @@
+from .blocks_keras import densenet_transition_block
+from .blocks_keras import densenet_conv_block
+from .blocks_keras import densenet_dense_block
+from .blocks_keras import squeeze_excitation_module
+from .blocks_keras import conv_block_YiboGao
+from .blocks_keras import attention_branch_YiboGao
+from .blocks_keras import RTA_block
+from .blocks_keras import spatial_attention_block_ZhangJin
+from .blocks_keras import temporal_attention_block_ZhangJin
+from .blocks_pytorch import ConvBlockYiboGao
+from .blocks_pytorch import AttentionBranchYiboGao
+from .blocks_pytorch import RTABlock
+from .blocks_pytorch import SqueezeAndExcitationModule
+from .blocks_pytorch import DenseNetTransitionBlock
+from .blocks_pytorch import DenseNetConvBlock
+from .blocks_pytorch import DenseNetDenseBlock
+from .blocks_pytorch import SpatialAttentionBlockZhangJin
+from .blocks_pytorch import TemporalAttentionBlockZhangJin
+from .data import get_mit_bih_segments
+from .data import read_mit_bih
+from .data import MIT_BIH
+from .models_keras import OhShuLih
+from .models_keras import KhanZulfiqar
+from .models_keras import ZhengZhenyu
+from .models_keras import HouBoroui
+from .models_keras import WangKejun
+from .models_keras import ChenChen
+from .models_keras import KimTaeYoung
+from .models_keras import GenMinxing
+from .models_keras import FuJiangmeng
+from .models_keras import ShiHaotian
+from .models_keras import HuangMeiLing
+from .models_keras import LihOhShu
+from .models_keras import GaoJunLi
+from .models_keras import WeiXiaoyan
+from .models_keras import KongZhengmin
+from .models_keras import YildirimOzal
+from .models_keras import CaiWenjuan
+from .models_keras import KimMinGu
+from .models_keras import HtetMyetLynn
+from .models_keras import ZhangJin
+from .models_keras import YaoQihang
+from .models_keras import YiboGao
+from .models_keras import HongTan
+from .models_pytorch import pyCNN_LSTM_BaseModule
+from .models_pytorch import OhShuLih
+from .models_pytorch import OhShuLih_Classifier
+from .models_pytorch import KhanZulfiqar
+from .models_pytorch import KhanZulfiqar_Classifier
+from .models_pytorch import ZhengZhenyu
+from .models_pytorch import ZhengZhenyu_Classifier
+from .models_pytorch import WangKejun
+from .models_pytorch import WangKejun_Classifier
+from .models_pytorch import ChenChen
+from .models_pytorch import ChenChen_Classifier
+from .models_pytorch import KimTaeYoung
+from .models_pytorch import KimTaeYoung_Classifier
+from .models_pytorch import GenMinxing
+from .models_pytorch import GenMinxing_Classifier
+from .models_pytorch import FuJiangmeng
+from .models_pytorch import FuJiangmeng_Classifier
+from .models_pytorch import ShiHaotian
+from .models_pytorch import ShiHaotian_Classifier
+from .models_pytorch import HuangMeiLing
+from .models_pytorch import HuangMeiLing_Classifier
+from .models_pytorch import LihOhShu
+from .models_pytorch import LiOhShu_Classifier
+from .models_pytorch import GaoJunLi
+from .models_pytorch import GaoJunLi_Classifier
+from .models_pytorch import WeiXiaoyan
+from .models_pytorch import WeiXiaoyan_Classifier
+from .models_pytorch import KongZhengmin
+from .models_pytorch import KongZhengmin_Classifier
+from .models_pytorch import YildirimOzal
+from .models_pytorch import CaiWenjuan
+from .models_pytorch import HtetMyetLynn
+from .models_pytorch import ZhangJin
+from .models_pytorch import ZhangJin_Classifier
+from .models_pytorch import YaoQihang
+from .models_pytorch import YaoQihangClassifier
+from .models_pytorch import YiboGao
+from .models_pytorch import YiboGaoClassifier
+from .models_pytorch import HongTan
+from .models_pytorch import HongTan_Classifier
+from .utils import TimeDistributed
+from .utils import flip_indices_for_conv_to_lstm
+from .utils import flip_indices_for_conv_to_lstm_reshape
+from .utils import check_inputs
+from .utils import full_convolution
+
+__all__ = ['densenet_transition_block',
+            'densenet_conv_block',
+            'densenet_dense_block',
+            'squeeze_excitation_module',
+            'conv_block_YiboGao',
+            'attention_branch_YiboGao',
+            'RTA_block',
+            'spatial_attention_block_ZhangJin',
+            'temporal_attention_block_ZhangJin',
+            'ConvBlockYiboGao',
+            'AttentionBranchYiboGao',
+            'RTABlock',
+            'SqueezeAndExcitationModule',
+            'DenseNetTransitionBlock',
+            'DenseNetConvBlock',
+            'DenseNetDenseBlock',
+            'SpatialAttentionBlockZhangJin',
+            'TemporalAttentionBlockZhangJin',
+            'get_mit_bih_segments',
+            'read_mit_bih',
+            'MIT_BIH',
+            'OhShuLih',
+            'KhanZulfiqar',
+            'ZhengZhenyu',
+            'HouBoroui',
+            'WangKejun',
+            'ChenChen',
+            'KimTaeYoung',
+            'GenMinxing',
+            'FuJiangmeng',
+            'ShiHaotian',
+            'HuangMeiLing',
+            'LihOhShu',
+            'GaoJunLi',
+            'WeiXiaoyan',
+            'KongZhengmin',
+            'YildirimOzal',
+            'CaiWenjuan',
+            'KimMinGu',
+            'HtetMyetLynn',
+            'ZhangJin',
+            'YaoQihang',
+            'YiboGao',
+            'HongTan',
+            'pyCNN_LSTM_BaseModule',
+            'OhShuLih',
+            'OhShuLih_Classifier',
+            'KhanZulfiqar',
+            'KhanZulfiqar_Classifier',
+            'ZhengZhenyu',
+            'ZhengZhenyu_Classifier',
+            'WangKejun',
+            'WangKejun_Classifier',
+            'ChenChen',
+            'ChenChen_Classifier',
+            'KimTaeYoung',
+            'KimTaeYoung_Classifier',
+            'GenMinxing',
+            'GenMinxing_Classifier',
+            'FuJiangmeng',
+            'FuJiangmeng_Classifier',
+            'ShiHaotian',
+            'ShiHaotian_Classifier',
+            'HuangMeiLing',
+            'HuangMeiLing_Classifier',
+            'LihOhShu',
+            'LiOhShu_Classifier',
+            'GaoJunLi',
+            'GaoJunLi_Classifier',
+            'WeiXiaoyan',
+            'WeiXiaoyan_Classifier',
+            'KongZhengmin',
+            'KongZhengmin_Classifier',
+            'YildirimOzal',
+            'CaiWenjuan',
+            'HtetMyetLynn',
+            'ZhangJin',
+            'ZhangJin_Classifier',
+            'YaoQihang',
+            'YaoQihangClassifier',
+            'YiboGao',
+            'YiboGaoClassifier',
+            'HongTan',
+            'HongTan_Classifier',
+            'TimeDistributed',
+            'flip_indices_for_conv_to_lstm',
+            'flip_indices_for_conv_to_lstm_reshape',
+            'check_inputs',
+            'full_convolution']
